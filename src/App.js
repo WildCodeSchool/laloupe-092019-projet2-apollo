8d0aaa3e6dad2ec3829168d103c380c6de82e0a1
rfcniblon <51241155+rfcniblon@users.noreply.github.com>
2019-11-04 10:49:30 +0100

    Update App.js
    ok

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
 import React from 'react';
 import './App.css';
 
-
-
-
-
-
-
 import ContactForm from './components/contact/ContactForm';
 function App() {
     return (
         <div className="App">
-           <ContactForm />
+          <Switch>
+        <Route path="/ContactUs" component={ContactForm} />
+      </Switch>
+        
         </div>
     );
 }
 export default App;
-
-
-
-
-
-
-
-
-
-
-<div>
-    <Route path="/ContactUs" component={ContactForm} /> 
-</div>
