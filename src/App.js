a6ee4aa21a2215eaa6d7dce377eeb2d8cb254d58
BArnaudet <baptiste.arnaudet@orange.fr>
2019-11-04 10:38:22 +0100

    add ContactForm

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+
+
+
+
+
+import ContactForm from './components/contact/ContactForm';
+function App() {
+    return (
+        <div className="App">
+           <ContactForm />
+        </div>
+    );
+}
 export default App;
+
+
+
+
+
+
+
+
+
+
+<div>
+    <Route path="/ContactUs" component={ContactForm} /> 
+</div>
